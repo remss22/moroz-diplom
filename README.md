8f5820df89c45cbc2c6c9d1de7b913df2f7e7418
Evgeniy Urkin <ue@roistat.com>
2017-05-16 00:59:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+project
+=======
+
+A Symfony project created on May 16, 2017, 12:38 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="show")
+     */
+    public function showAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function addAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/monitoring/{id}", name="monitoring", requirements={"id": "\d+"})
+     */
+    public function monitoringAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete", requirements={"id": "\d+"})
+     */
+    public function deleteAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
